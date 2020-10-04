bb8b93e9924905ac6aa2a261e89f912ed137670c
Kimi-Toshiko <kimitoshiko@gmail.com>
2020-10-05 02:17:24 +0500

    refactor: erase debugging functions

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,3 @@
-// MAIN
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
@@ -17,41 +16,3 @@ module.exports = function getSeason(date) {
   else
     throw new Error('Input is not a date');
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// EXAMPLE
-// function getSeason(date) {
-//   if (date == null || date == undefined) {
-//     console.log('Unable to determine the time of year!');
-//     return 'Unable to determine the time of year!';
-//   }
-//   else if (date instanceof Date && !isNaN(date.valueOf())) {
-//     if (date.getMonth() === 11 || date.getMonth() < 2)
-//       return 'winter';
-//     else if (date.getMonth() >= 2 && date.getMonth() < 5)
-//       return 'spring';
-//     else if (date.getMonth() >= 5 && date.getMonth() < 8)
-//       return 'summer';
-//     else
-//       return 'autumn';
-//   }
-//   else
-//     throw new Error('Input is not a date');
-// }
-
-// const dateObj = new Date(2134, 2, 17, 11, 27, 4, 321);
-// getSeason();
