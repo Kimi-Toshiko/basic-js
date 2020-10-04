899331afbef02df04c6c5fe66f0762583b505d12
Kimi-Toshiko <kimitoshiko@gmail.com>
2020-10-04 22:37:11 +0500

    feat: add solition for extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,50 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options, finalResult) {
+  let res = '';
+
+  const pureStr = () => {
+    if(typeof str === 'string')
+      return str;
+    else if (str === null)
+      return 'null';
+    else
+      return str.toString();
+  };
+
+  const repeatTimes = () => options.repeatTimes ? options.repeatTimes : 0;
+  const separator  = () => options.separator ? options.separator : '+';
+  const addition = () => {
+    if (typeof options.addition === 'string')
+      return options.addition;
+    else if (options.addition === null)
+      return 'null';
+    else
+      return options.addition.toString();
+  };
+  const additionSeparator = () => options.additionSeparator ? options.additionSeparator : '';
+  const additionRepeatTimes = () => options.additionRepeatTimes ? options.additionRepeatTimes : 1;
+
+  if (options.addition !== undefined) {
+    let strWithAddition = `${pureStr()}${addition()}`;
+
+    for (let i = 0; i < additionRepeatTimes() - 1; i++) {
+      strWithAddition += `${additionSeparator()}${addition()}`;
+    }
+
+    res += strWithAddition;
+
+    for (let i = 0; i < repeatTimes() - 1; i++) {
+      res += `${separator()}${strWithAddition}`;   
+    }
+  }
+  else {
+    res += pureStr();
+
+    for (let i = 0; i < repeatTimes() - 1; i++) {
+      res += `${separator()}${pureStr()}`;     
+    }
+  }
+
+  return res;
 };
-  
